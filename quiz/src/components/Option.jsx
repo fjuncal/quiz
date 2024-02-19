fd97edd5460887d5e71869cfadddb7856c55aaee
Fellipe Juncal <fjuncal7@gmail.com>
2024-02-19 19:30:19 -0300

    Implementando o componente de opções das perguntas

---
+++ quiz/src/components/Option.jsx
@@ -0,0 +1,18 @@
+import { useContext } from 'react'
+import './Option.css'
+import { QuizContext } from '../context/quiz'
+
+
+// eslint-disable-next-line react/prop-types
+function Option({option}) {
+
+    const [quizState, dispatch ] = useContext(QuizContext);
+
+  return (
+    <div>
+      <p>{option}</p>
+    </div>
+  )
+}
+
+export default Option
