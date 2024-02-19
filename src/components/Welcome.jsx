665f7bcfbe8d7a8e95ab414d61e0f935ac09c017
Fellipe Juncal <fjuncal7@gmail.com>
2024-02-19 20:50:00 -0300

    acertando tamanho da foto e botão centralizado

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react'
-import Quiz from '../img/quiz.svg'
+//import Quiz from '../img/quiz.svg'
 import './Welcome.css'
 import { QuizContext } from '../context/quiz'
 import EuNath from '../img/eu-natinha.jpeg'
@@ -14,7 +14,9 @@ const Welcome = () => {
         <h2> Seja bem-vindo </h2>
         <p>Clique no botão abaixo para começar</p>
         <img src={EuNath} alt='Início do Quiz'/>
-        <button onClick={() => dispatch({type: "CHANGE_STATE"})}>Iniciar</button>
+        <div>
+          <button onClick={() => dispatch({type: "CHANGE_STATE"})}>Iniciar</button>
+        </div>
     </div>
   )
 }
