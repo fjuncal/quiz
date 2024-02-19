09f6b4b5fb1ad01fac5ff07bfa8d880249a7cc00
Fellipe Juncal <fjuncal7@gmail.com>
2024-02-19 18:45:21 -0300

    pegando a pergunta do estado atual

--- quiz/src/components/Question.jsx
+++ quiz/src/components/Question.jsx
@@ -4,11 +4,12 @@ import { QuizContext } from "../context/quiz"
 
 const Question = () => {
     const [quizState, dispatch] = useContext(QuizContext)
+    const currentQuestion = quizState.questions[quizState.currentQuestion];
     console.log(quizState);
   return (
     <div id="question">
       <p>Pergunta {quizState.currentQuestion + 1} de {quizState.questions.length}</p>
-      <h2>Pergunta atual</h2>
+      <h2>{currentQuestion.question}</h2>
       <div id="options-container">
         <p>Opções</p>
       </div>
